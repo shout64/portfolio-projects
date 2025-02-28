0f12ead6109b6fae4a398e9c596a529c24934da0
Devin Chaney <135728867+shout64@users.noreply.github.com>
2025-02-28 10:32:47 -0600

    Update housing_report.sql

--- SQL Report Examples/housing_report.sql
+++ SQL Report Examples/housing_report.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT
-	gvcas.id_num as 'ID Number',
+	vas.id_num as 'ID Number',
 	nm.FIRST_NAME as 'First Name',
 	nm.LAST_NAME as 'Last Name',
 	nm.EMAIL_ADDRESS as 'Email Address',
@@ -10,18 +10,18 @@ SELECT DISTINCT
 	sar.MealPlan as 'Meal Plan',
 	(SELECT STRING_AGG(st.SPORTS_CDE_DESCRIPTION, ', ')
 		FROM SPORTS_TRACKING_V st
-		WHERE st.ID_NUM = gvcas.id_num
+		WHERE st.ID_NUM = vas.id_num
 		AND (CAST(st.YR_CDE as varchar) + st.TRM_CDE) = csm.SESS_CDE
 		GROUP BY st.id_num
 	) as Sports
-FROM GVC_VW_ACTIVE_STUDENTS gvcas
-	LEFT OUTER JOIN Name_Master nm		   ON gvcas.id_num = nm.ID_NUM
-	INNER JOIN		STUDENT_DIV_MAST sdm   ON gvcas.id_num = sdm.ID_NUM
-	INNER JOIN		STUDENT_MASTER sm	   ON gvcas.id_num = sm.ID_NUM
+FROM VW_ACTIVE_STUDENTS vas
+	LEFT OUTER JOIN Name_Master nm	       ON vas.id_num = nm.ID_NUM
+	INNER JOIN	STUDENT_DIV_MAST sdm   ON vas.id_num = sdm.ID_NUM
+	INNER JOIN	STUDENT_MASTER sm      ON vas.id_num = sm.ID_NUM
 	LEFT OUTER JOIN SAApplicantRoster sar  ON nm.ID_NUM = sar.StudentIdNum 
 	LEFT OUTER JOIN CM_SESSION_MSTR csm    ON csm.GOID = sar.SessionGoid 
-	LEFT OUTER JOIN STUD_TERM_SUM_DIV stsd ON gvcas.id_num = stsd.ID_NUM and (CAST(stsd.YR_CDE as varchar) + stsd.TRM_CDE) = csm.SESS_CDE
-	LEFT OUTER JOIN SPORTS_TRACKING_V st   ON nm.ID_NUM = st.ID_NUM AND gvcas.id_num = st.ID_NUM AND (CAST(st.YR_CDE as varchar) + st.TRM_CDE) = csm.SESS_CDE
+	LEFT OUTER JOIN STUD_TERM_SUM_DIV stsd ON vas.id_num = stsd.ID_NUM AND (CAST(stsd.YR_CDE as varchar) + stsd.TRM_CDE) = csm.SESS_CDE
+	LEFT OUTER JOIN SPORTS_TRACKING_V st   ON nm.ID_NUM = st.ID_NUM AND vas.id_num = st.ID_NUM AND (CAST(st.YR_CDE as varchar) + st.TRM_CDE) = csm.SESS_CDE
 WHERE 
 	stsd.DIV_CDE = 'UG'
 	AND stsd.transaction_sts <> 'd'
