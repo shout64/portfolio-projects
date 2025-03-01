68877afe67f75744c466a913208a7984fe561b5d
Devin Chaney <135728867+shout64@users.noreply.github.com>
2025-03-01 11:49:15 -0600

    Update spacing on open_path_export.sql

--- SQL Report Examples/open_path_export.sql
+++ SQL Report Examples/open_path_export.sql
@@ -17,17 +17,17 @@ FROM
 	INNER JOIN dbo.year_Term_table ytt on sch.trm_cde = ytt.trm_cde AND sch.yr_cde = ytt.YR_CDE
 WHERE
 	GETDATE() between dateadd(day,-3,
-		(SELECT  min(first_begin_dte)
-		FROM  student_crs_hist sch1
-			INNER JOIN  section_master sm on sch1.crs_cde = sm.crs_cde AND sch1.yr_cde = sm.yr_cde AND sch1.trm_cde = sm.trm_cde
-		WHERE  sch1.ID_NUM = sch.ID_NUM
-			AND sch1.TRM_CDE != 'TR'))
-	AND
-		(SELECT max(last_end_dte)
-		FROM  student_crs_hist sch1
-			INNER JOIN  section_master sm on sch1.crs_cde = sm.crs_cde AND sch1.yr_cde = sm.yr_cde AND sch1.trm_cde = sm.trm_cde
-		WHERE  sch1.ID_NUM = sch.ID_NUM
-		AND sch1.TRM_CDE != 'TR')
+			(SELECT  min(first_begin_dte)
+			FROM  student_crs_hist sch1
+				INNER JOIN  section_master sm on sch1.crs_cde = sm.crs_cde AND sch1.yr_cde = sm.yr_cde AND sch1.trm_cde = sm.trm_cde
+			WHERE  sch1.ID_NUM = sch.ID_NUM
+				AND sch1.TRM_CDE != 'TR'))
+		AND
+			(SELECT max(last_end_dte)
+			FROM  student_crs_hist sch1
+				INNER JOIN  section_master sm on sch1.crs_cde = sm.crs_cde AND sch1.yr_cde = sm.yr_cde AND sch1.trm_cde = sm.trm_cde
+			WHERE  sch1.ID_NUM = sch.ID_NUM
+			AND sch1.TRM_CDE != 'TR')
 	AND nmu.PROX_ID IS NOT NULL
 	AND bm.EMPLOYEE_OF_COLLEG = 'N'
 	--AND nm.ID_NUM = xxxxxxxxx --remove comment for testing single user
