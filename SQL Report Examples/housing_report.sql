ba2f5e4575606a8f54d4db1f01594e3657eaad63
Devin Chaney <135728867+shout64@users.noreply.github.com>
2025-10-14 09:07:15 -0400

    Update housing_report spacing

--- SQL Report Examples/housing_report.sql
+++ SQL Report Examples/housing_report.sql
@@ -16,14 +16,18 @@ SELECT DISTINCT
 	) as Sports
 FROM VW_ACTIVE_STUDENTS vas
 	LEFT OUTER JOIN Name_Master nm	       ON vas.id_num = nm.ID_NUM
-	INNER JOIN	STUDENT_DIV_MAST sdm   ON vas.id_num = sdm.ID_NUM
-	INNER JOIN	STUDENT_MASTER sm      ON vas.id_num = sm.ID_NUM
+	INNER JOIN	STUDENT_DIV_MAST sdm       ON vas.id_num = sdm.ID_NUM
+	INNER JOIN	STUDENT_MASTER sm          ON vas.id_num = sm.ID_NUM
 	LEFT OUTER JOIN SAApplicantRoster sar  ON nm.ID_NUM = sar.StudentIdNum 
 	LEFT OUTER JOIN CM_SESSION_MSTR csm    ON csm.GOID = sar.SessionGoid 
-	LEFT OUTER JOIN STUD_TERM_SUM_DIV stsd ON vas.id_num = stsd.ID_NUM AND (CAST(stsd.YR_CDE as varchar) + stsd.TRM_CDE) = csm.SESS_CDE
-	LEFT OUTER JOIN SPORTS_TRACKING_V st   ON nm.ID_NUM = st.ID_NUM AND vas.id_num = st.ID_NUM AND (CAST(st.YR_CDE as varchar) + st.TRM_CDE) = csm.SESS_CDE
+	LEFT OUTER JOIN STUD_TERM_SUM_DIV stsd ON vas.id_num = stsd.ID_NUM
+		AND (CAST(stsd.YR_CDE as varchar) + stsd.TRM_CDE) = csm.SESS_CDE
+	LEFT OUTER JOIN SPORTS_TRACKING_V st   ON nm.ID_NUM = st.ID_NUM
+		AND vas.id_num = st.ID_NUM
+		AND (CAST(st.YR_CDE as varchar) + st.TRM_CDE) = csm.SESS_CDE
 WHERE 
 	stsd.DIV_CDE = 'UG'
 	AND stsd.transaction_sts <> 'd'
 	AND (csm.SESS_CDE = :SessionCode or csm.SESS_CDE IS NULL)
 ORDER BY nm.LAST_NAME
+
